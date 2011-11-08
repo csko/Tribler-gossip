2df8f467340574cd84f110c62859370c97f6efe6
csko <cskornel@gmail.com>
2011-11-08 14:45:15 +0100

    Another bugfix.

--- Tribler/community/gossiplearningframework/abstractcommunity.py
+++ Tribler/community/gossiplearningframework/abstractcommunity.py
@@ -76,7 +76,7 @@ class AbstractGossipCommunity(Community):
 #                                   meta.distribution.implement(self.claim_global_time()),
                                    meta.destination.implement(),
 #                                   meta.destination.implement(True),
-                                   meta.payload.implement(self._message)))
+                                   meta.payload.implement(message)))
           self._dispersy.store_update_forward(send_messages, False, False, True)
 #          self._dispersy.store_update_forward(send_messages, True, True, True) # For testing
 
