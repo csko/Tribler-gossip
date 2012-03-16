e4684106baa1806a898a46cb3e6df583a4a2775d
csko <cskornel@gmail.com>
2012-03-16 03:25:23 +0100

    Small fixes. P2Pegsos model.

---
+++ Tribler/community/gossiplearningframework/models/p2pegasos.py
@@ -0,0 +1,40 @@
+from models import GossipLearningModel
+
+if __debug__:
+    from Tribler.Core.dispersy.dprint import dprint
+
+class P2PegasosModel(GossipLearningModel):
+
+    def __init__(self):
+        super(P2PegasosModel, self).__init__()
+
+        # Initial model
+        self.w = [0, 0, 0, 0, 0]
+        self.age = 0
+
+    def update(self, x, y):
+        """Update the model with a new training example."""
+        # Set up some variables.
+        label = -1.0 if y == 0 else 1.0
+
+        self.age = self.age + 1
+        lam = 0.0001
+        rate = 1.0 / (self.age * lam)
+
+        is_sv = label * sum([self.w[i] * x[i] for i in range(len(self.w))]) < 1.0
+        max_dim = max(len(self.w), len(x))
+        for i in range(max_dim):
+          if is_sv:
+            self.w[i] = (1.0 - 1.0 / self.age) * self.w[i] + rate * label * x[i]
+          else:
+            self.w[i] = (1.0 - 1.0 / self.age) * self.w[i]
+
+    def predict(self, x):
+        """
+        Compute the inner product of the hyperplane and the instance as a
+        prediction.
+        """
+
+        inner_product = sum([self.w[i] * x[i] for i in range(len(self.w))])
+        return 1.0 if inner_product > 0.0 else 0.0
+
